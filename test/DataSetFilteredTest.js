780fcbbbbee29af551af1fe2aa4561b6ce80d390
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2015-06-02 17:15:46 +0200

    Add a test for filtering data set

---
+++ test/DataSetFilteredTest.js
@@ -0,0 +1,59 @@
+import expect from 'expect.js';
+
+import { AdapterManager } from 'mosaic-adapters';
+import { DataSet, DataSetFiltered } from '..';
+
+describe('DataSetFiltered', function() {
+    let adapters = new AdapterManager();
+    function check(first, second){
+        expect(JSON.stringify(first)).to.eql(JSON.stringify(second));
+    }
+    
+    it('should contain a subset of elements from the parent DataSet', function(done) {
+        let dataSet = new DataSet({adapters});
+        let subSet = new DataSetFiltered({dataSet, filter: function(r, pos) {
+            return pos % 2 === 0;
+        }});
+        let subsetUpdateIntent = null;
+        subSet.on('update', function(intent){
+            subsetUpdateIntent = intent;
+        });
+        let index = {};
+        let list = [];
+        let count = 10;
+        for (let i = 0; i < count; i++) {
+            let data = {
+                id : 'id-' + i,
+                msg: 'item - ' + i
+            };
+            list.push(data);
+            index[data.id] = data;
+        }
+        expect(subsetUpdateIntent).to.be(null);
+        dataSet.setAll(list).then(function(){
+            expect(dataSet.length).to.eql(count);
+            expect(!!subsetUpdateIntent).to.be(true);
+            return subsetUpdateIntent.promise;
+        }).then(function(){
+            expect(subSet.length).to.eql(count / 2);
+            let positions = [];
+            for (let i = 0; i < list.length; i++) {
+                let r1 = dataSet.children[i];
+                let id = 'id-' + i;
+                let r2 = subSet.byId(id);
+                if (i % 2 === 0) {
+                    if (dataSet.children[i] !== subSet.children[i / 2]) {
+                        expect().fail();
+                    }
+                    if (r1 !== r2) {
+                        expect().fail();
+                    }
+                } else {
+                    if (r2) {
+                        expect().fail();
+                    }
+                }
+            }
+        }).then(done, done);
+    });
+});
