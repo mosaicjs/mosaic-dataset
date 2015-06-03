adb40d5b3107d45516cca044241c159e2ca93df7
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2015-06-03 17:01:57 +0200

    Simplify the DataSetFiltered class

--- dist/index.js
+++ dist/index.js
@@ -765,7 +765,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	         * of objects when the parent set changes.
 	         */
 	        value: function _handleMainDataSetUpdate() {
-	            this.resources = [].concat(this.dataSet.resources);
+	            this.resources = this.dataSet.resources;
 	        }
 	    }]);
 
@@ -787,8 +787,6 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
@@ -804,20 +802,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	var _DerivativeDataSet3 = _interopRequireDefault(_DerivativeDataSet2);
 
 	var DataSetFiltered = (function (_DerivativeDataSet) {
-	    function DataSetFiltered(options) {
-	        var _get2;
-
-	        for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-	            args[_key - 1] = arguments[_key];
-	        }
-
+	    function DataSetFiltered() {
 	        _classCallCheck(this, DataSetFiltered);
 
-	        (_get2 = _get(Object.getPrototypeOf(DataSetFiltered.prototype), 'constructor', this)).call.apply(_get2, [this, options].concat(args));
-	        if (!options) return;
-	        var filter = options.filter;
-	        if (typeof filter === 'function') {
-	            this._filter = filter;
+	        if (_DerivativeDataSet != null) {
+	            _DerivativeDataSet.apply(this, arguments);
 	        }
 	    }
 
@@ -826,14 +815,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	    _createClass(DataSetFiltered, [{
 	        key: '_handleMainDataSetUpdate',
 	        value: function _handleMainDataSetUpdate() {
-	            var dataSet = this.dataSet;
-	            var result = dataSet.resources.filter(this._filter, this);
-	            this.resources = result;
-	        }
-	    }, {
-	        key: '_filter',
-	        value: function _filter(r) {
-	            return !!r;
+	            var filter = this._getOptionsValue('filter');
+	            if (filter) {
+	                this.resources = this.dataSet.resources.filter(filter, this);
+	            } else {
+	                this.resources = this.dataSet.resources;
+	            }
 	        }
 	    }]);
 
